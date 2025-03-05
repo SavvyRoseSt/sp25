c4c4cef252ca5e02f7807e0db76acece7269fba3
SavvyRoseSt <savstewa@iu.edu>
2025-03-04 19:31:36 -0500

    lessons

---
+++ homework-3/lessons.js
@@ -0,0 +1,12 @@
+console.log("list of instruments");
+const ul = "list of instruments";
+
+const instrument = ["Piano", "Guitar", "Violin", "Drums", "Saxophone", "Flute"];
+
+console.log(instrument);
+
+for (let i = 0; i < instrument.length; i++) {
+    console.log(instrument[i]); // Outputs each number in the array
+  }
+
+  
