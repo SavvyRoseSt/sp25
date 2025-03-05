192ee0f765dc2edde0cf464bc4a944327880e7e4
SavvyRoseSt <savstewa@iu.edu>
2025-03-04 20:03:19 -0500

    Average Score

---
+++ homework-3/scores.js
@@ -0,0 +1,11 @@
+console.log("Score");
+const ul = "Score";
+
+const grade = [95, 72, 88, 45, 60, 79, 82, 91, 50, 38]
+
+for (let i = 0; i < grade.length; i++) {
+  console.log(grade[i]); // Loop
+}
+console.log("Average Score")
+let Testish = 70;
+console.log(Testish)
