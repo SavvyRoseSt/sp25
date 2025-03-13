f450bdd05d667141b43a3e0a3dc6a15cff7d72fe
SavvyRoseSt <savstewa@iu.edu>
2025-03-12 21:53:10 -0400

    update

---
+++ homework-4/js/home.js
@@ -0,0 +1,21 @@
+console.log("Cozy home");
+
+console.log("2 + 7 =", 2 + 7);
+
+console.log("'2' + '2' =", "2" + "2");
+
+console.log("My name is " + "Dil");
+
+console.log("2 + 2 = ", 2 + Number("2"));
+
+if (true) {
+    console.log("Always true");
+} else {
+    console.error("This shouldn't Happen");
+}
+
+let fishcake = 0.75;
+
+if (fishcake >= 0.5) {
+    
+}
---
+++ homework-4/js/object.js
@@ -0,0 +1,5 @@
+//ref Variables (ref the DOM / html Page)
+const cribULRef = document.querySelector("#crib");
+
+//Data Variable
+const crib = []
